667cf8684cc1478a1815eb25a308ca5ceb7d3cc7
leohalldevall <leo.halldevall@elev.ga.ntig.se>
2023-08-31 11:48:03 +0200

    modul 1

---
+++ modul1.py
@@ -0,0 +1,27 @@
+print("hello world")
+#1 hello world
+print("'En älg är inte ren'")
+#2 Ett citat
+print(""" 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum 
+      Lorem Ipsum """)
+#3 lorem ipsum 10 gånger
+print("En anka är inte en häst \n men en häst är en fågel")
+#4 Skriv två saker i samma print med \n
+print("fy"+"siker!")
+#5 sät ihop två ord med +
+print("häst","sko")
+#6 använd , för att sätta ihop ord
+namn=input("namn")
+print("hej " + namn + " hur gammal är du")
+ålder = int(input("ålder"))
+if (ålder > 17):
+    print("Du är gammal")
